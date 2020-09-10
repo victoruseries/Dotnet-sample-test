5df97023a69c7b4b5041c8feef42edeba859dbd1
victoruseries <victoruseries@gmail.com>
2020-09-10 00:32:19 -0600

    Initialization of the console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dotnet_sample_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
